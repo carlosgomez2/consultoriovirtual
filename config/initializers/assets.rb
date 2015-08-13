569f0b708e5f45dd9fb0153231858b9155bd99a5
Carlos Gomez <carlosgomez.deb@gmail.com>
2015-08-13 17:36:14 -0500

    creando ramas

---
+++ config/initializers/assets.rb
@@ -0,0 +1,12 @@
+# Javascript 
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( classie.js )
+Rails.application.config.assets.precompile += %w( cbpAnimatedHeader.js )
+Rails.application.config.assets.precompile += %w( jqBootstrapValidation.js )
+Rails.application.config.assets.precompile += %w( contact_me.js )
+Rails.application.config.assets.precompile += %w( freelancer.js )
+# CSS3
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( freelancer.css )
+Rails.application.config.assets.precompile += %w( font-awesome.css )
